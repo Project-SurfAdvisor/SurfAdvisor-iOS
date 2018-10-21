6cb1130d222445584703c02791a3e2971171f370
pkse1213 <pkse1213@naver.com>
2018-10-21 13:36:37 +0900

    detail 뷰

---
+++ SurfAdvisor/ViewControllers/DetailVC.swift
@@ -0,0 +1,37 @@
+//
+//  DeatilVC.swift
+//  SurfAdvisor
+//
+//  Created by 박세은 on 2018. 10. 21..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class DetailVC: UIViewController {
+    @IBOutlet weak var infoView: UIView!
+    @IBOutlet weak var forcastView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
